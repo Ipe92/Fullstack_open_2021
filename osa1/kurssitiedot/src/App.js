0f563c3f816feff3bdf17ea9df8a7c5df2f44724
Iippo Leppänen <21137181@edu.riveria.fi>
2021-05-26 10:20:41 +0300

    javascript tehtavät 1b kesken

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,20 +1,53 @@
-import React from 'react';
-import Header from './Header';
-import Content from './Content';
-import Total from './Total';
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
 
 const App = () => {
+  
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
-      <Header course={course} />
-      <Content part={part} />
-      <Total exercise={exercise} />
+      ...
     </div>
   )
 }
 
-export default App;
+    return (
+    <div>
+      <Header course={...} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+    )
+};
+
+// const App = () => {
+
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content part={part} />
+//       <Total exercise={exercise} />
+//     </div>
+//   )
+// }
+
+// export default App;
 
 /*
  <div>
