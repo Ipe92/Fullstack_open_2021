0c520cce7eddaca48551e062c1bb08f6923b23de
ipe <iippol@protonmail.com>
2021-09-10 14:01:07 +0300

    unicafe step6

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -2,24 +2,29 @@ import React, { useState } from "react";
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}> {text} </button>;
 
+const StatisticLine = ({ text, value }) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    );
+};
+
 const Statistics = ({ good, neutral, bad }) => {
-    if (good === 0 && neutral === 0 && bad === 0) {
-        return (
-            <div>
-                <h1>statistics</h1>
-                No feedback given
-            </div>
-        );
-    }
     return (
         <div>
             <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {good + bad + neutral}</p>
-            <p>average {(good - bad) / (good + neutral + bad)}</p>
-            <p>positive {(good / (good + neutral + bad)) * 100} %</p>
+            <table>
+                <tbody>
+                    <StatisticLine text="good" value={good} />
+                    <StatisticLine text="neutral" value={neutral} />
+                    <StatisticLine text="bad" value={bad} />
+                    <StatisticLine text="all" value={good + neutral + bad} />
+                    <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
+                    <StatisticLine text="positive" value={(good / (good + neutral + bad)) * 100} />
+                </tbody>
+            </table>
         </div>
     );
 };
