7378f7e37b86ddecdcf356d6f6fdae7260b24b4f
ipe <iippol@protonmail.com>
2021-09-10 14:47:26 +0300

    anekdootit tehty

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 
-const App = () => {
+const App = (props) => {
+    const MostVotes = (props) => {
+        return (
+            <div>
+                {props.anecdotes[props.points.reduce((iMax, x, i, arr) => (x > arr[iMax] ? i : iMax), 0)]}
+                <p>has {Math.max(...props.points)} votes</p>
+            </div>
+        );
+    };
+
+    const Button = (props) => {
+        return <button onClick={props.handleClick}>{props.text}</button>;
+    };
+
     const anecdotes = [
         "If it hurts, do it more often",
         "Adding manpower to a late software project makes it later!",
@@ -11,8 +24,38 @@ const App = () => {
     ];
 
     const [selected, setSelected] = useState(0);
+    const [points, setPoints] = useState(Array.apply(null, new Array(6)).map(Number.prototype.valueOf, 0));
 
-    return <div>{anecdotes[selected]}</div>;
+    const handleRandomClick = (props) => {
+        const rnd = Math.floor(Math.random() * Math.floor(6));
+
+        console.log("random: ", rnd);
+        setSelected(rnd);
+    };
+
+    const handleVoteClick = (props) => {
+        const copy = [...points];
+        copy[props.selected] += 1;
+        setPoints(copy);
+    };
+
+    return (
+        <div>
+            <h1>Anecdote of the day</h1>
+            {anecdotes[selected]}
+            <p>has {points[selected]} votes</p>
+
+            <div>
+                <Button handleClick={() => handleVoteClick({ selected })} text="vote" />
+                <Button handleClick={() => handleRandomClick()} text="next anecdote" />
+            </div>
+
+            <div>
+                <h1>Anecdote with most votes</h1>
+                <MostVotes anecdotes={anecdotes} points={points} />
+            </div>
+        </div>
+    );
 };
 
 export default App;
