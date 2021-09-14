d21abf209dd34df71331f82cad4f7e562edea5f2
ipe <iippol@protonmail.com>
2021-09-14 18:25:16 +0300

    part 2-2

--- osa2/part2-1/src/App.js
+++ osa2/part2-1/src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Note from "./components/Note";
 
 const App = (props) => {
-    console.log(props);
-    const { notes } = props;
+    const [notes, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState("a new note...");
+
+    const addNote = (event) => {
+        event.preventDefault();
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            import: Math.random() > 0.5,
+            id: notes.length + 1,
+        };
+        setNotes(notes.concat(noteObject));
+        setNewNote("");
+    };
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value);
+        setNewNote(event.target.value);
+    };
+
     return (
         <div>
             <h1>Notes</h1>
@@ -12,6 +30,10 @@ const App = (props) => {
                     <Note key={note.id} note={note} />
                 ))}
             </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
         </div>
     );
 };
