f22c4ea22765040aad6fed2f07cce2f3fda27732
ipe <iippol@protonmail.com>
2021-09-16 11:40:33 +0300

    part 2-4

--- osa2/part2-1/src/App.js
+++ osa2/part2-1/src/App.js
@@ -1,51 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
 const App = (props) => {
-    const [notes, setNotes] = useState(props.notes);
-    const [newNote, setNewNote] = useState("a new note...");
-    const [showAll, setshowAll] = useState(true);
+	const [notes, setNotes] = useState([]);
+	const [newNote, setNewNote] = useState("a new note...");
+	const [showAll, setshowAll] = useState(true);
 
-    const addNote = (event) => {
-        event.preventDefault();
-        const noteObject = {
-            content: newNote,
-            date: new Date().toISOString(),
-            import: Math.random() > 0.5,
-            id: notes.length + 1,
-        };
-        setNotes(notes.concat(noteObject));
-        setNewNote("");
-    };
+	useEffect(() => {
+		console.log("effect");
+		axios.get("http://localhost:3001/notes").then((response) => {
+			console.log("promise fulfilled");
+			setNotes(response.data);
+		});
+	}, []);
+	console.log("render", notes.length, "notes");
 
-    const handleNoteChange = (event) => {
-        console.log(event.target.value);
-        setNewNote(event.target.value);
-    };
+	const addNote = (event) => {
+		event.preventDefault();
+		const noteObject = {
+			content: newNote,
+			date: new Date().toISOString(),
+			import: Math.random() > 0.5,
+			id: notes.length + 1,
+		};
+		setNotes(notes.concat(noteObject));
+		setNewNote("");
+	};
 
-    const notesToShow = showAll
-        ? notes
-        : notes.filter((note) => note.important);
+	const handleNoteChange = (event) => {
+		console.log(event.target.value);
+		setNewNote(event.target.value);
+	};
 
-    return (
-        <div>
-            <h1>Notes</h1>
-            <div>
-                <button onClick={() => setshowAll(!showAll)}>
-                    show {showAll ? "important" : "all"}
-                </button>
-            </div>
-            <ul>
-                {notesToShow.map((note) => (
-                    <Note key={note.id} note={note} />
-                ))}
-            </ul>
-            <form onSubmit={addNote}>
-                <input value={newNote} onChange={handleNoteChange} />
-                <button type="submit">save</button>
-            </form>
-        </div>
-    );
+	const notesToShow = showAll
+		? notes
+		: notes.filter((note) => note.important);
+
+	return (
+		<div>
+			<h1>Notes</h1>
+			<div>
+				<button onClick={() => setshowAll(!showAll)}>
+					show {showAll ? "important" : "all"}
+				</button>
+			</div>
+			<ul>
+				{notesToShow.map((note) => (
+					<Note key={note.id} note={note} />
+				))}
+			</ul>
+			<form onSubmit={addNote}>
+				<input value={newNote} onChange={handleNoteChange} />
+				<button type="submit">save</button>
+			</form>
+		</div>
+	);
 };
 
 export default App;
