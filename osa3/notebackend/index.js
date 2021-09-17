fccdcd11fd5845f679ce7f21b0c9c7c25c0783d6
ipe <iippol@protonmail.com>
2021-09-17 17:41:02 +0300

    rest client

--- osa3/notebackend/index.js
+++ osa3/notebackend/index.js
@@ -22,12 +22,25 @@ let notes = [
 	},
 ];
 
-app.get("/", (req, res) => {
-	res.send("<h1>Hello World!</h2>");
+app.get("/api/notes/", (request, response) => {
+	response.json(notes);
 });
 
-app.get("/api/notes", (req, res) => {
-	res.json(notes);
+app.get("/api/notes/:id", (request, response) => {
+	const id = Number(request.params.id);
+	const note = notes.find((note) => note.id === id);
+
+	if (note) {
+		response.json(note);
+	} else {
+		response.status(404).end();
+	}
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+	const id = Number(request.params.id);
+	const note = notes.filter((note) => note.id === id);
+	response.status(204).end();
 });
 
 const PORT = 3001;
