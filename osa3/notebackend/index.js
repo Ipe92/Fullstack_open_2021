0a9d0ffea91f070048408e782453168b4cdb2490
Iippo Leppänen <21137181@edu.riveria.fi>
2021-09-23 12:36:50 +0300

    update small cleanup

--- osa3/notebackend/index.js
+++ osa3/notebackend/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
-
 const cors = require("cors");
-app.use(cors());
 
 app.use(express.static("build"));
-
+app.use(cors());
 app.use(express.json());
 
 const requestLogger = (request, response, next) => {
