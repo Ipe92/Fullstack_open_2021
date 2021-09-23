13201074eb96c82baea1337419a8969fb3475fb5
Iippo Leppänen <21137181@edu.riveria.fi>
2021-09-23 10:50:20 +0300

    note changed and heroku pushed

--- osa2/part2-1/src/services/notes.js
+++ osa2/part2-1/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://locahost:3001/notes";
+const baseUrl = "http://locahost:3001/api/notes";
 
 const getAll2 = () => {
 	const request = axios.get(baseUrl);
