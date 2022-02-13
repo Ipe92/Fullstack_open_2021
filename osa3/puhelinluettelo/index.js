5b0af164aaec0a5a0fe38b742cee67bcf7e86a94
iippol <iippol@protonmail.com>
2022-02-13 20:53:16 +0200

    3.17*: puhelinluettelo ja tietokanta, step5

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -108,6 +108,21 @@ app.delete("/api/persons/:id", (req, res, next) => {
 		});
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+	const body = req.body;
+
+	const person = {
+		name: body.name,
+		number: body.number,
+	};
+
+	Person.findByIdAndUpdate(req.params.id, person, { new: true })
+		.then((updatedPerson) => {
+			res.json(Person.format(updatedPerson));
+		})
+		.catch((error) => next(error));
+});
+
 const unknownEndpoint = (req, res) => {
 	res.status(404).send({ error: "unknown endpoint" });
 };
